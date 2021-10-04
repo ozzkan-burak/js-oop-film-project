205dd771e3d028f9bfa57deab05ae8fa3d487c85
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-04 23:01:36 +0300

    remove from UI method

--- js/project.js
+++ js/project.js
@@ -2,6 +2,7 @@ const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
 const urlElement = document.querySelector("#url");
+const cardBody = document.querySelectorAll('.card-body')[1];
 
 // UI Object Start
 const ui = new UI();
@@ -20,7 +21,10 @@ function eventListeners(){
   document.addEventListener('DOMContentLoaded', function() {
     let films = storage.getFilmsFromStorage();
     ui.loadAllFilms(films);
-  })
+  });
+
+  cardBody.addEventListener("click", deleteFilm);
+
 }
 
 function addfilm(e){
@@ -44,4 +48,14 @@ function addfilm(e){
   ui.clearInputs(titleElement, directorElement, urlElement);
 
   e.preventDefault();
+}
+
+function deleteFilm(e) {
+
+  if(e.target.id === "delete-film") {
+
+    ui.deleteFilmFromUI(e.target);
+
+  }
+  
 }
