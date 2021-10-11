b936be57b2210e4284121bedbb6aabe92681aef6
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-12 00:10:38 +0300

    project end

--- ES6/js/project.js
+++ ES6/js/project.js
@@ -5,14 +5,6 @@ const urlElement = document.querySelector("#url");
 const cardBody = document.querySelectorAll('.card-body')[1];
 const deleteAllFilms = document.getElementById('clear-films');
 
-// UI Object Start
-const ui = new UI();
-
-// create Storage
-
-const storage = new Storage();
-
-
 // All Events Upload
 eventListeners();
 
@@ -20,8 +12,8 @@ function eventListeners(){
   form.addEventListener("submit", addfilm);
 
   document.addEventListener('DOMContentLoaded', function() {
-    let films = storage.getFilmsFromStorage();
-    ui.loadAllFilms(films);
+    let films = Storage.getFilmsFromStorage();
+    UI.loadAllFilms(films);
   });
 
   cardBody.addEventListener("click", deleteFilm);
@@ -38,19 +30,19 @@ function addfilm(e){
   const id = uuidv4();
 
   if(title === "" || director === "" || url === ""){
-    ui.displayMessages("Tüm alanların doldurulması zorunludur", "danger");
+    UI.displayMessages("Tüm alanların doldurulması zorunludur", "danger");
 
   } else {
-    ui.displayMessages("Film başarı ile eklenmiştir", "success");
+    UI.displayMessages("Film başarı ile eklenmiştir", "success");
 
     // Create New Film
     const newFilm = new Film(title, director, url, id);
 
-    storage.addFilmToStorage(newFilm);
-    ui.addFilmToUI(newFilm); // Add film for UI
+    Storage.addFilmToStorage(newFilm);
+    UI.addFilmToUI(newFilm); // Add film for UI
   }
 
-  ui.clearInputs(titleElement, directorElement, urlElement);
+  UI.clearInputs(titleElement, directorElement, urlElement);
 
   e.preventDefault();
 };
@@ -59,7 +51,7 @@ function deleteFilm(e) {
 
   if(e.target.id === "delete-film") {
 
-    ui.deleteFilmFromUI(e.target);
+    UI.deleteFilmFromUI(e.target);
 
   }
 };
@@ -68,8 +60,8 @@ function deleteFilm(e) {
 function deleteAll() {
 
   const deleteID = document.querySelector('#films');
-  ui.deleteAllFilmFromUI(deleteID);
-  ui.displayMessages("Listenizi zenginleştirin, yeni filmler ekleyin.", "warning");
+  UI.deleteAllFilmFromUI(deleteID);
+  UI.displayMessages("Listenizi zenginleştirin, yeni filmler ekleyin.", "warning");
 
-  storage.deleteAllFilmFromStorage();
+  Storage.deleteAllFilmFromStorage();
 };
